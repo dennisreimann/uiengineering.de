ff33c3a04ee8f20dc0babaa1e065479c398a3266
Dennis Reimann <mail@dennisreimann.de>
2016-11-26 14:12:24 +0100

    🔨 Update structure, pages and build
    - add navigation
    - improve header for non-hero use case on content pages
    - add episode template
    - separate homepage content into dedicated pages
    
    TODO:
    - improve copy text on content pages
    - discuss navigation flow and user actions
    - add podcast feed, subscribe button and player

---
+++ gulpfile.babel.js
@@ -0,0 +1,170 @@
+import gulp from 'gulp';
+import gulpLoadPlugins from 'gulp-load-plugins';
+import runSequence from 'run-sequence';
+import autoprefixer from 'autoprefixer';
+import mqpacker from 'css-mqpacker';
+import csswring from 'csswring';
+import BrowserSync from 'browser-sync';
+import templateHelper from './lib/templateHelper';
+
+const p = gulpLoadPlugins();
+const browserSync = BrowserSync.create();
+const baseUrl = 'https://www.uiengineering.de';
+
+const paths = {
+  src: 'src',
+  dest: 'dist',
+  rev: ['dist/**/*.{css,js,map,svg,jpg,png,gif,woff,woff2}'],
+  copy: ['src/{fonts,images,svgs}/**/*', 'src/{podcast}/mp3s/*.mp3', 'src/favicon.ico', 'src/.htaccess'],
+  pages: ['src/pages/**/*.jade'],
+  styles: ['src/styles/**/*.styl'],
+  scripts: ['src/scripts/**/*.js'],
+  sitemap: ['dist/**/*.html'],
+  optimizeImages: ['src/{images,svgs}/**/*'],
+  episodes: ['src/podcast/*.md'],
+  templates: 'src/templates/*.jade',
+  episodeTemplate: 'src/templates/episode.jade',
+  episodesBasepath: 'podcast',
+};
+
+const dest = (folder = '') => gulp.dest(`${paths.dest}/${folder}`);
+
+const mvbConf = {
+  glob: paths.episodes,
+  template: paths.episodeTemplate,
+  permalink(article) {
+    return `/${paths.episodesBasepath}/${article.id}.html`;
+  },
+  grouping(articles) {
+    const byYear = {};
+
+    articles.forEach(function(article) {
+      const year = article.date.toISOString().replace(/-.*/, '');
+      if (!byYear[year]) { byYear[year] = []; }
+      byYear[year].push(article);
+    });
+
+    // year
+    const articlesByYear = [];
+    Object.keys(byYear).reverse().forEach(year => articlesByYear.push({year, articles: byYear[year]}));
+
+    // groups
+    return {
+      byYear: articlesByYear
+    };
+  }
+};
+
+const templateData = file => ({
+  h: templateHelper.createHelper(file, baseUrl)
+});
+
+const jadeOpts = {
+  basedir: 'src/templates/',
+  pretty: true,
+};
+
+const buildHtml = (src, dst) =>
+  gulp.src(src)
+    .pipe(p.plumber())
+    .pipe(p.mvb(mvbConf))
+    .pipe(p.data(templateData))
+    .pipe(p.jade(jadeOpts))
+    .pipe(p.minifyHtml({empty: true}))
+    .pipe(dest(dst))
+    .pipe(browserSync.stream())
+
+const feedWithTemplate = (template, folder) =>
+  gulp.src(`src/feed/${template}.jade`)
+    .pipe(p.plumber())
+    .pipe(p.mvb(mvbConf))
+    .pipe(p.data(templateData))
+    .pipe(p.jade(jadeOpts))
+    .pipe(p.rename({extname: '.xml'}))
+    .pipe(dest(folder))
+
+gulp.task('feed', () => feedWithTemplate('podcast'));
+
+gulp.task('copy', cb =>
+  gulp.src(paths.copy)
+    .pipe(dest())
+    .pipe(browserSync.stream())
+);
+
+gulp.task('pages', () => buildHtml(paths.pages));
+gulp.task('episodes', () => buildHtml(paths.episodes, paths.episodesBasepath));
+
+gulp.task('scripts', () =>
+  gulp.src(paths.scripts)
+    .pipe(p.plumber())
+    .pipe(p.babel())
+    .pipe(p.uglify())
+    .pipe(dest('scripts'))
+    .pipe(browserSync.stream({match: '**/*.js'}))
+);
+
+gulp.task('styles', () =>
+  gulp.src(paths.styles)
+    .pipe(p.plumber())
+    .pipe(p.stylus({
+      paths: ['src/styles/lib'],
+      import: ['mediaQueries', 'variables']
+    }))
+    .pipe(p.concat('main.css'))
+    .pipe(p.postcss([
+      mqpacker,
+      autoprefixer({browsers: ['last 2 versions']}),
+      csswring
+    ]))
+    .pipe(dest('styles'))
+    .pipe(browserSync.stream({match: '**/*.css'}))
+);
+
+gulp.task('browserSync', () =>
+  browserSync.init({
+    open: false,
+    server: {
+      baseDir: paths.dest
+    }
+  })
+);
+
+gulp.task('optimizeImages', () =>
+  gulp.src(paths.optimizeImages)
+    .pipe(p.imagemin())
+    .pipe(gulp.dest('src'))
+);
+
+gulp.task('revAssets', () => {
+  const revAll = new p.revAll({prefix: assetHost});
+  return gulp.src(paths.rev)
+    .pipe(revAll.revision())
+    .pipe(p.revDeleteOriginal())
+    .pipe(dest())
+    .pipe(revAll.manifestFile())
+    .pipe(dest())
+});
+
+gulp.task('sitemap', () =>
+  gulp.src(paths.sitemap)
+    .pipe(p.sitemap({
+      siteUrl: baseUrl,
+      changefreq: 'weekly'
+    }))
+    .pipe(dest())
+);
+
+gulp.task('watch', () => {
+  gulp.watch(paths.copy, ['copy']);
+  gulp.watch(paths.styles, ['styles']);
+  gulp.watch(paths.scripts, ['scripts']);
+  gulp.watch(paths.episodeTemplate, ['episodes']);
+  gulp.watch(paths.templates, ['episodes', 'pages']);
+  gulp.watch(paths.pages).on('change', file => buildHtml(file.path));
+  gulp.watch(paths.episodes).on('change', file => buildHtml(file.path, paths.episodesBasepath));
+});
+
+gulp.task('build', cb => runSequence('styles', ['copy', 'pages', 'episodes', 'scripts'], cb));
+gulp.task('develop', cb => runSequence('build', ['watch', 'browserSync'], cb));
+gulp.task('rev', cb => runSequence('revAssets', ['pages', 'episodes'], cb));
+gulp.task('production', cb => runSequence('build', 'rev', 'sitemap', cb));
---
+++ src/podcast/2016-11-28-s01e01-atomic-design.md
@@ -0,0 +1,21 @@
+---
+title: Atomic Design
+subtitle: Die Chemie von Web Interfaces
+shortId: S01E01
+tags:
+  - podcast
+  - uiengineering
+  - atomic design
+  - ui systeme
+  - patternlab
+---
+
+In der ersten Folge des UI engineering Podcast geht es um Atomic Design: Was ist das genau und warum eignet es sich für die Konzeption von Web Interfaces? Wir sprechen über die Grundlagen, Komponentisierung, Wiederverwendbarkeit und Dinge, die zu beachten sind.
+
+<!-- more -->
+
+### Shownotes
+
+- [Atomic Design Intro](http://bradfrost.com/blog/post/atomic-web-design/ )
+- [Atomic Design Buch](http://atomicdesign.bradfrost.com/)
+- [Designing Modular UI Systems Via Style Guide-Driven Development](https://www.smashingmagazine.com/2016/06/designing-modular-ui-systems-via-style-guide-driven-development/)
